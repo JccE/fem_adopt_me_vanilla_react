7a9b9f03d523ccc79374c29134adbdde4ef5204d
jcce <toshajo99@gmail.com>
2019-10-16 13:33:26 -0500

    add npm scripts, prettier, etc..

--- src/App.js
+++ src/App.js
@@ -8,52 +8,54 @@
 
 // using destructuring
 // pulls out the names of objects to make them avail as variable names
-const Pet = ({name, animal, breed}) => {
-  return React.createElement("div",{}, [
-      React.createElement("h1", {}, name),
-      React.createElement("h2", {}, animal),
-      React.createElement("h2", {}, breed)
-    ]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
+// just like puts or Rails.info
+// throw new Error("lol");
+// const App = () => {
+//   return React.createElement(
+//     "div",
+//     // the attributes to pass in
+//     {id: "something-important"},
+//     // the children of the div. one or more
+//     // React.createElement("h1", {}, "Adopt Me!")
+//     [
+//       React.createElement("h1", {}, "Adopt Me!"),
+//       React.createElement(Pet, {}, []),
+//       React.createElement(Pet),
+//       React.createElement(Pet)
+//     ]
+//  );
+// };
 
-    // just like puts or Rails.info
-    // throw new Error("lol");
-    // const App = () => {
-    //   return React.createElement(
-    //     "div",
-    //     // the attributes to pass in
-    //     {id: "something-important"},
-    //     // the children of the div. one or more
-    //     // React.createElement("h1", {}, "Adopt Me!")
-    //     [
-    //       React.createElement("h1", {}, "Adopt Me!"),
-    //       React.createElement(Pet, {}, []),
-    //       React.createElement(Pet),
-    //       React.createElement(Pet)
-    //     ]
-    //  );
-    // };
+const App = () => {
+  return React.createElement(
+    "div",
+    // the attributes to pass in
+    { id: "something-important" },
+    // the children of the div. one or more
+    // React.createElement("h1", {}, "Adopt Me!")
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+    ]
+  );
+};
 
-
-    const App = () => {
-      return React.createElement(
-        "div",
-        // the attributes to pass in
-        {id: "something-important"},
-        // the children of the div. one or more
-        // React.createElement("h1", {}, "Adopt Me!")
-        [
-          React.createElement("h1", {}, "Adopt Me!"),
-          React.createElement(Pet, {name: "Luna", animal: "Dog", breed:"Havanese"}),
-          React.createElement(Pet, {name: "Pepper", animal: "Bird", breed:"Cockatiel"}),
-          React.createElement(Pet, {name: "Doink", animal: "Cat", breed:"Mixed"})
-        ]
-     );
-    };
-
-
-    ReactDOM.render(
-      React.createElement(App),
-        document.getElementById("root")
-    );
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
