46c0183c77f1fc3bf2ad9a996dda9121f3ba9b74
jcce <toshajo99@gmail.com>
2019-10-23 18:47:49 -0500

    clean up

--- src/Details.js
+++ src/Details.js
@@ -1,31 +1,10 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 
-// class component
 class Details extends React.Component {
   state = { loading: true };
 
-  // set loading state
-  // constructor(props) {
-  //   // super says, call constructor on/all the way up to the React.Component
-  //   super(props);
-
-  //   // set inital state
-  //   // calling this.setState will update this.state
-  //   // it overrides and is additive
-  //   // it is shallow by nature, deeply nested states will get entirely overridden
-  //   this.state = {
-  //     loading: true
-  //   };
-  // }
-
-  // lifecycle methods
-  // componentDidMount is very similar to useEffect in that it runs with first started up but does not run anymore afterwards
   componentDidMount() {
-    // when we render the details page, render with the associated id
-    // anything passed in from the parents will come from this.props.
-    // this.props is immutable. cannot be changed/ read-only
-
     pet.animal(this.props.id).then(
       ({ animal }) => {
         this.setState({
@@ -35,23 +14,19 @@ class Details extends React.Component {
           description: animal.description,
           media: animal.photos,
           breed: animal.breeds.primary,
-          // when api comes back. set it to false
+
           loading: false
         });
       },
-      // log errors in .then
-      // write better errors than console.error!!
+
       console.error
     );
   }
 
-  // every class component must have a render method
   render() {
     if (this.state.loading) {
-      // eventually make all pretty
       return <h1>loading ...</h1>;
     }
-    // pull attributes out of this.state
     const { animal, breed, location, description, name } = this.state;
 
     return (
@@ -66,15 +41,5 @@ class Details extends React.Component {
     );
   }
 }
-
-// function component
-// const Details = props => {
-//   // return <h1>Hey Bitches</h1>;
-//   return (
-//     <pre>
-//       <code>{JSON.stringify(props, null, 4)}</code>
-//     </pre>
-//   );
-// };
 
 export default Details;
