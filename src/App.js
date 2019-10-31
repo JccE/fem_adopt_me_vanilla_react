e517695f0e0a6336b3b72871725fd2868106a5e8
jcce <toshajo99@gmail.com>
2019-10-31 17:26:57 -0500

    example of code spliting with example of importing heavy library

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
 // import { Router, Link } from "@reach/router";
 import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import "babel-polyfill";
-import Details from "./Details";
+// import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
+
+// details is now a placeholder component that wont load the code till later
+// parcel wraps this in a specific bundle
+const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -15,10 +19,12 @@ const App = () => {
       <ThemeContext.Provider value={themeHook}>
         <div>
           <NavBar />
-          <Router>
-            <SearchParams path="/" />
-            <Details path="details/:id" />
-          </Router>
+          <Suspense fallback={<h1>loading route ... </h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
